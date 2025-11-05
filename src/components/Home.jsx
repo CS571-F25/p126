5b74e057ef3a09dc2e66db8fb8c71751ed35297c
jahalverson3 <jahalverson3@wisc.edu>
2025-11-04 22:08:15 -0600

    Changes for Initial Publish

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 export default function Home(props) {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Initial Publish</h1>
+      <h2>/#/about has another page as in the tutorial</h2>
     </div>
   );
 }
